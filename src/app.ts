1aa57cb6b07b7c35f027e05de75af660c41276fd
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-07 20:22:55 +0600

    Add cookie-parser middleware and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,19 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/modules/middlewares/errorHandler';
 import route from './app/router';
+import cookieParser from 'cookie-parser';
 const app: Application = express();
+
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+};
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser())
+app.use(cors(corsOptions));
 
 app.use('/api',route);
 
